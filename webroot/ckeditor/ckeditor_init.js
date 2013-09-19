1e6ecd79c941ee973cc1a07736602e3542afd063
Mirko Pagliai <mirko.pagliai@gmail.com>
2013-09-19 20:07:04 +0200

    added ckeditor scripts

---
+++ webroot/ckeditor/ckeditor_init.js
@@ -0,0 +1,29 @@
+$(function() {
+	$('textarea.wysiwyg').ckeditor({
+		disableNativeSpellChecker: false,
+		fontSize_sizes: '10/10px;11/11px;12/12px;14/14px;16/16px;18/18px;20/20px;22/22px;24/24px;26/26px;28/28px;30/30px;',
+		height: 375,
+		insertpre_class: false,
+		language: 'it',
+		removeButtons: 'Font',
+		removeDialogTabs: false,
+		tabSpaces: 4,
+		toolbarCanCollapse: true,
+		toolbarGroups: [
+			{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+			{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+			{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+			{ name: 'links' },
+			{ name: 'insert' },
+			{ name: 'forms' },
+			{ name: 'tools' },
+			{ name: 'others' },
+			'/',
+			{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+			{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+			{ name: 'styles' },
+			{ name: 'colors' },
+			{ name: 'about' }
+		]
+	});
+});
