271fca4f1e041f2715efea9cdc2cead2407e71c8
Mirko Pagliai <mirko.pagliai@gmail.com>
2014-01-02 18:09:01 +0100

    added description

--- Utility/FileArray.php
+++ Utility/FileArray.php
@@ -1,5 +1,33 @@
 <?php
+/**
+ * FileArray
+ *
+ * This file is part of MeTools.
+ *
+ * MeTools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * MeTools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with MeTools.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author		Mirko Pagliai <mirko.pagliai@gmail.com>
+ * @copyright	Copyright (c) 2013, Mirko Pagliai for Nova Atlantis Ltd
+ * @license		http://www.gnu.org/licenses/agpl.txt AGPL License
+ * @link		http://git.novatlantis.it Nova Atlantis Ltd
+ * @package		MeTools\Utility
+ */
 
+/**
+ * Provides several methods for using a text file as database.
+ * It can count, delete, edit, check, search, retrieve and insert data.
+ */
 class FileArray  {
 	/**
 	 * File content
