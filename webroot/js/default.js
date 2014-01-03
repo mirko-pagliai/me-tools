86b68c69761bec07ecd9a218e9337e434be5c419
Mirko Pagliai <mirko.pagliai@gmail.com>
2014-01-03 09:57:36 +0100

    added code to close the flash messages

--- webroot/js/default.js
+++ webroot/js/default.js
@@ -19,15 +19,21 @@
  * @license		http://www.gnu.org/licenses/agpl.txt AGPL License
  * @link		http://git.novatlantis.it Nova Atlantis Ltd
  */
+
+/**
+ * Closes a flash messages with an animation
+ */
+function close_flashMessage() {
+	$('#flashMessage').animate({opacity: '0'}, '800', function(){ $(this).slideUp('200'); });
+}
+
 $(function() {	
 	/**
 	 * When you click on "check/uncheck all", it changes the checkboxed state
 	 */
 	$('.checkboxes-list .checkAll, .checkboxes-list .uncheckAll').click(function(event) {
-		//Prevents the default event
 		event.preventDefault();
 		
-		//Gets checkboxes
 		var checkboxes = $(this).parents('.checkboxes-list').find('input[type="checkbox"]');
 		
 		//If you have clicked on "check all"
@@ -37,4 +43,18 @@ $(function() {
 		else
 			checkboxes.prop('checked', false);
 	});
+	
+	/**
+	 * Closes a flash message when clicking the close button
+	 */
+	$('#flashMessage .close').click(function() {
+		close_flashMessage();
+		//It requires "return false" to prevent the default behavior of jQuery
+		return false;
+	});
+	
+	/**
+	 * Closes automatically the flash messages after a preset time
+	 */
+	setTimeout(close_flashMessage, 3500);
 });
