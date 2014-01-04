129d654171c141a81bcb5d312d97c57648d3c143
Mirko Pagliai <mirko.pagliai@gmail.com>
2014-01-04 16:41:00 +0100

    updated build script

--- build.sh
+++ build.sh
@@ -5,16 +5,16 @@ echo "Please, enter the version number: "
 read version
 version="metools-$version"
 
-# Create build/last-version
-rm -r -f build/last-version
-mkdir -p build/last-version
+# Create build/MeTools
+rm -r -f build/MeTools
+mkdir -p build/MeTools
 
-# Copy in build/last-version
+# Copy in build/MeTools
 cp -R Config/ Console/ Controller/ Lib/ Locale/ Model/ Utility/ \
-Vendor/ View/ webroot/ COPYNG README.md build/last-version
+Vendor/ View/ webroot/ COPYNG README.md build/MeTools
 
-# Enter build/last-version
-cd build/last-version
+# Enter build/MeTools
+cd build/MeTools
 
 # Delete uncompressed files
 rm -f webroot/css/datepicker.css webroot/css/default.css \
@@ -26,5 +26,5 @@ webroot/js/default.js webroot/js/slugify.js
 cd ../
 
 # Create the tar archive
-tar -czf $version.tar.gz last-version/ --strip-components 1
+tar -czf $version.tar.gz MeTools/*
 echo "The file $version.tar.gz was created"
