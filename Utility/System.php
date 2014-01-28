52bff1e92cad3ed27908c80b8f7c138eb710fed0
Mirko Pagliai <mirko.pagliai@gmail.com>
2014-01-28 22:52:00 +0100

    fixed spaces and tabs

--- Utility/System.php
+++ Utility/System.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * System utility
  *
@@ -33,168 +34,168 @@
  * </code>
  */
 class System {
-	/**
-	 * Checks if an Apache module is active
-	 * @param string $module Name of the module to be checked
-	 * @return boolean TRUE if the module is enabled, FALSE otherwise
-	 * @uses getApacheModules() to get Apache's modules
-	 */
-	public static function checkApacheModule($module) {
-		return in_array($module, self::getApacheModules());
-	}
-	
-	/**
-	 * Checks if the cache and all its subdirectories are readable and writable
-	 * @return boolean TRUE if if the cache and all its subdirectories are readable and writable, FALSE otherwise
-	 */
-	public static function checkCache() {
-		//Cache default directories
-		$default = array(CACHE, CACHE.'models', CACHE.'persistent', CACHE.'views');
-		
-		//Directories in the cache
-		$cache = new Folder(CACHE);
-		$cache = $cache->tree(CACHE, FALSE, 'dir');
-		
-		foreach(array_unique(am($cache, $default)) as $dir) {
-			if(!is_readable($dir) || !is_writable($dir))
-				return FALSE;
-		}
-		
-		return TRUE;
-	}
+    /**
+     * Checks if an Apache module is active
+     * @param string $module Name of the module to be checked
+     * @return boolean TRUE if the module is enabled, FALSE otherwise
+     * @uses getApacheModules() to get Apache's modules
+     */
+    public static function checkApacheModule($module) {
+        return in_array($module, self::getApacheModules());
+    }
 
-	/**
-	 * Checks the cache status (if it's enabled)
-	 * @return boolean TRUE if the cache is enabled, FALSE otherwise
-	 */
-	public static function checkCacheStatus() {
-		return !Configure::read('Cache.disable');
-	}
-	
-	/**
-	 * Checks if the current version of PHP is equal to or greater than the required version
-	 * @param string $required_version Required version of PHP
-	 * @return boolean TRUE if the current version of PHP is equal to or greater than the required version, FALSE otherwise
-	 * @uses getPhpVersion() to get the current version of PHP
-	 */
-	public static function checkPhpVersion($required_version) {		
-		return version_compare(self::getPhpVersion(), $required_version, '>=');
-	}
-	
-	/**
-	 * Checks if a PHP extension is enabled
-	 * @param string $extension Name of the extension to be checked
-	 * @return boolean TRUE if the extension is enabled, FALSE otherwise
-	 */
-	public static function checkPhpExtension($extension) {
-		return extension_loaded($extension);
-	}
-	
-	/**
-	 * Checks if the TMP and all its subdirectories are readable and writable
-	 * @return boolean TRUE if the TMP and all its subdirectories are readable and writable, FALSE otherwise
-	 */
-	public static function checkTmp() {		
-		$tmp = new Folder(TMP);
-		
-		foreach($tmp->tree(TMP, FALSE, 'dir') as $dir) {
-			if(!is_readable($dir) || !is_writable($dir))
-				return FALSE;
-		}
-		
-		return TRUE;
-	}
-	
-	/**
-	 * Checks if the thumbnail directory is readable and writable
-	 * @return boolean TRUE if the thumbnail directory is readable and writable, FALSE otherwise
-	 */
-	public static function checkThumbs() {
-		return is_readable(TMP.'thumbs') && is_writable(TMP.'thumbs');
-	}
-	
-	/**
-	 * Clear the cache
-	 * @return boolean TRUE if the cache was successfully cleared, FALSE otherwise
-	 */
-	public static function clearCache() {
-		return Cache::clear(FALSE);
-	}
-	
-	/**
-	 * Clear thumbnails
-	 * @return boolean TRUE if thumbnails were successfully cleared, FALSE otherwise
-	 */
-	public static function clearThumbs() {
-		$dir = new Folder($tmp = TMP.'thumbs');
-		$files = $dir->find('.*');
-		
-		$success = TRUE;
-		
-		foreach($files as $file) {
-			$file = new File($tmp.DS.$file);
-			if(!$file->delete() && $success)
-				$success = FALSE;
-		}
-		
-		return $success;
-	}
-	
-	/**
-	 * Gets the Apache modules list
-	 * @return array Modules list
-	 */
-	public static function getApacheModules() {
-		return apache_get_modules();
-	}
-	
-	/**
-	 * Gets the cache size
-	 * @return int Cache size
-	 */
-	public static function getCacheSize() {
-		$cache = new Folder(CACHE);
-		return $cache->dirsize();
-	}
-	
-	/**
-	 * Gets the CakePHP version
-	 * @return string CakePHP version
-	 */
-	public static function getCakeVersion() {
-		return Configure::version();
-	}
-	
-	/**
-	 * Gets the MeTools version
-	 * @return string MeTools version
-	 */
-	public static function getMeToolsVersion() {
-		return file_get_contents(APP.'version');
-	}
-	
-	/**
-	 * Gets the PHP extensions list
-	 * @return array Extensions list
-	 */
-	public static function getPhpExtensions() {
-		return get_loaded_extensions();
-	}
-	
-	/**
-	 * Gets the current version of PHP
-	 * @return string Current version of PHP
-	 */
-	public static function getPhpVersion() {
-		return PHP_VERSION;
-	}
-	
-	/**
-	 * Gets the thumbnails size
-	 * @return int Thumbnails size
-	 */
-	public static function getThumbsSize() {
-		$thumbs = new Folder(TMP.'thumbs');
-		return $thumbs->dirsize();
-	}
+    /**
+     * Checks if the cache and all its subdirectories are readable and writable
+     * @return boolean TRUE if if the cache and all its subdirectories are readable and writable, FALSE otherwise
+     */
+    public static function checkCache() {
+        //Cache default directories
+        $default = array(CACHE, CACHE.'models', CACHE.'persistent', CACHE.'views');
+
+        //Directories in the cache
+        $cache = new Folder(CACHE);
+        $cache = $cache->tree(CACHE, FALSE, 'dir');
+
+        foreach(array_unique(am($cache, $default)) as $dir) {
+            if(!is_readable($dir) || !is_writable($dir))
+                return FALSE;
+        }
+
+        return TRUE;
+    }
+
+    /**
+     * Checks the cache status (if it's enabled)
+     * @return boolean TRUE if the cache is enabled, FALSE otherwise
+     */
+    public static function checkCacheStatus() {
+        return !Configure::read('Cache.disable');
+    }
+
+    /**
+     * Checks if the current version of PHP is equal to or greater than the required version
+     * @param string $required_version Required version of PHP
+     * @return boolean TRUE if the current version of PHP is equal to or greater than the required version, FALSE otherwise
+     * @uses getPhpVersion() to get the current version of PHP
+     */
+    public static function checkPhpVersion($required_version) {
+        return version_compare(self::getPhpVersion(), $required_version, '>=');
+    }
+
+    /**
+     * Checks if a PHP extension is enabled
+     * @param string $extension Name of the extension to be checked
+     * @return boolean TRUE if the extension is enabled, FALSE otherwise
+     */
+    public static function checkPhpExtension($extension) {
+        return extension_loaded($extension);
+    }
+
+    /**
+     * Checks if the TMP and all its subdirectories are readable and writable
+     * @return boolean TRUE if the TMP and all its subdirectories are readable and writable, FALSE otherwise
+     */
+    public static function checkTmp() {
+        $tmp = new Folder(TMP);
+
+        foreach($tmp->tree(TMP, FALSE, 'dir') as $dir) {
+            if(!is_readable($dir) || !is_writable($dir))
+                return FALSE;
+        }
+
+        return TRUE;
+    }
+
+    /**
+     * Checks if the thumbnail directory is readable and writable
+     * @return boolean TRUE if the thumbnail directory is readable and writable, FALSE otherwise
+     */
+    public static function checkThumbs() {
+        return is_readable(TMP.'thumbs') && is_writable(TMP.'thumbs');
+    }
+
+    /**
+     * Clear the cache
+     * @return boolean TRUE if the cache was successfully cleared, FALSE otherwise
+     */
+    public static function clearCache() {
+        return Cache::clear(FALSE);
+    }
+
+    /**
+     * Clear thumbnails
+     * @return boolean TRUE if thumbnails were successfully cleared, FALSE otherwise
+     */
+    public static function clearThumbs() {
+        $dir = new Folder($tmp = TMP.'thumbs');
+        $files = $dir->find('.*');
+
+        $success = TRUE;
+
+        foreach($files as $file) {
+            $file = new File($tmp.DS.$file);
+            if(!$file->delete() && $success)
+                $success = FALSE;
+        }
+
+        return $success;
+    }
+
+    /**
+     * Gets the Apache modules list
+     * @return array Modules list
+     */
+    public static function getApacheModules() {
+        return apache_get_modules();
+    }
+
+    /**
+     * Gets the cache size
+     * @return int Cache size
+     */
+    public static function getCacheSize() {
+        $cache = new Folder(CACHE);
+        return $cache->dirsize();
+    }
+
+    /**
+     * Gets the CakePHP version
+     * @return string CakePHP version
+     */
+    public static function getCakeVersion() {
+        return Configure::version();
+    }
+
+    /**
+     * Gets the MeTools version
+     * @return string MeTools version
+     */
+    public static function getMeToolsVersion() {
+        return file_get_contents(APP.'version');
+    }
+
+    /**
+     * Gets the PHP extensions list
+     * @return array Extensions list
+     */
+    public static function getPhpExtensions() {
+        return get_loaded_extensions();
+    }
+
+    /**
+     * Gets the current version of PHP
+     * @return string Current version of PHP
+     */
+    public static function getPhpVersion() {
+        return PHP_VERSION;
+    }
+
+    /**
+     * Gets the thumbnails size
+     * @return int Thumbnails size
+     */
+    public static function getThumbsSize() {
+        $thumbs = new Folder(TMP.'thumbs');
+        return $thumbs->dirsize();
+    }
 }
