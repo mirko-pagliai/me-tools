40d28a146d62c118d8dd7c30ec9c97dfd8f0efc7
Mirko Pagliai <mirko.pagliai@gmail.com>
2014-02-05 22:31:08 +0100

    added MeExceptionRenderer class and error views

---
+++ Lib/Error/MeExceptionRenderer.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Exception Renderer.
+ *
+ * This file is part of MeTools.
+ *
+ * MeTools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * MeTools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with MeTools.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author		Mirko Pagliai <mirko.pagliai@gmail.com>
+ * @copyright	Copyright (c) 2013, Mirko Pagliai for Nova Atlantis Ltd
+ * @license		http://www.gnu.org/licenses/agpl.txt AGPL License
+ * @link		http://git.novatlantis.it Nova Atlantis Ltd
+ * @package		MeTools\Lib\Error
+ * @see			http://api.cakephp.org/2.0/class-ExceptionRenderer.html
+ */
+
+/**
+ * This class is used only in order to use the error views from the MeTools plugin.
+ * 
+ * In `core.php` file, you have to change the configuration of the exceptions as follows:
+ * <code>
+ * Configure::write('Exception', array(
+ *     'handler' => 'ErrorHandler::handleException',
+ *     'renderer' => 'MeTools.MeExceptionRenderer',
+ *     'log' => true
+ * ));
+ * </code>
+ * 
+ * Rewrites {@link http://api.cakephp.org/2.0/class-ExceptionRenderer.html ExceptionRenderer}
+ */
+class MeExceptionRenderer extends ExceptionRenderer {
+	/**
+	 * This method is used only in order to use the error views from the MeTools plugin.
+	 * @param Exception $exception The exception to get a controller for.
+	 * @return Controller
+	 */
+	protected function _getController($exception) {
+		$controller = parent::_getController($exception);
+		$controller->plugin = 'MeTools';
+		return $controller;
+	}
+}
---
+++ View/Errors/error400.ctp
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Error 400.
+ *
+ * This file is part of MeTools.
+ *
+ * MeTools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * MeTools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with MeTools.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author		Mirko Pagliai <mirko.pagliai@gmail.com>
+ * @copyright	Copyright (c) 2013, Mirko Pagliai for Nova Atlantis Ltd
+ * @license		http://www.gnu.org/licenses/agpl.txt AGPL License
+ * @link		http://git.novatlantis.it Nova Atlantis Ltd
+ * @package		MeTools\View\Errors
+ */
+?>
+
+<div class="error-page">
+	<?php
+		echo $this->Html->tag('h2', __('Oops...').' '.$this->Html->tag('small', __('This is really embarrassing')));
+		echo $this->Html->tag('h4', __('Houston, we have an error!'));
+		
+		echo $this->Html->para('error-text text-danger bg-danger', sprintf('<strong>%s:</strong> %s', __('Error'), $name));
+		
+		$html = $this->Html->para(NULL, __('The requested address was not found on this server:'));
+		$html .= $this->Html->tag('pre', Router::url($url, true));
+		echo $this->Html->div('error-info', $html);
+		
+		echo $this->Html->para(NULL, __('Have found a bug? Consider the possibility report it.'));
+
+		if(Configure::read('debug'))
+			echo $this->Html->div('stack-trace', $this->element('exception_stack_trace'));
+	?>
+</div>
