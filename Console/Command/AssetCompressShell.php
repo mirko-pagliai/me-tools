25c74b28cdeae094e1a2caabde3b31f53fcf1703
Mirko Pagliai <mirko.pagliai@gmail.com>
2014-10-16 17:25:34 +0200

    It checks if the output directory is writable

--- Console/Command/AssetCompressShell.php
+++ Console/Command/AssetCompressShell.php
@@ -79,6 +79,10 @@ class AssetCompressShell extends MeToolsAppShell {
 				$this->error(sprintf('%s doesn\'t exists or is not readable', $file));
 		}
 		
+		//Checks if the output directory is writable
+		if(!is_writable(dirname($output)))
+				$this->error(sprintf('%s doesn\'t exists or is not writeable', $output));
+		
 		//If the output file already exists and has not been used the "force" option, it asks if it should be overwritten
 		if(file_exists($output) && empty($this->params['force']) && $this->in(sprintf('The file %s already exists. Do you want to overwrite it?', $output), array('y', 'n'), 'y') === 'n') {
 			$this->out('Ok, i\'m exiting...');
