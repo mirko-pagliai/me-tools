a2b0dfe2696c31b95c5c98713e213fb9d2676187
Mirko Pagliai <mirko.pagliai@gmail.com>
2014-10-25 17:39:13 +0200

    fixed

--- webroot/ckeditor/ckeditor_init.js
+++ webroot/ckeditor/ckeditor_init.js
@@ -9,6 +9,7 @@ $(function() {
 		disableNativeSpellChecker: false,
 		fontSize_sizes: '10/10px;11/11px;12/12px;14/14px;16/16px;18/18px;20/20px;22/22px;24/24px;26/26px;28/28px;30/30px;',
 		height: 375,
+		image2_alignClasses: [ 'pull-left', 'text-center', 'pull-right' ],
 		insertpre_class: false,
 		language: 'en',
 		removeButtons: 'Font',
