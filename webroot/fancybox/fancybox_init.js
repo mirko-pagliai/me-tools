b0294154ad684429ca283ab561186c465ca51d68
Mirko Pagliai <mirko.pagliai@gmail.com>
2015-01-31 12:06:04 +0100

    Right click disabled by default

---
+++ webroot/fancybox/fancybox_init.js
@@ -0,0 +1,22 @@
+/**
+ * This file is part of MeTools.
+ * @author		Mirko Pagliai <mirko.pagliai@gmail.com>
+ * @copyright	Copyright (c) 2015, Mirko Pagliai for Nova Atlantis Ltd
+ * @license		http://www.gnu.org/licenses/agpl.txt AGPL License
+ */
+$(document).ready(function() {
+	$(".fancybox").fancybox({
+		closeBtn: false,
+		prevEffect: 'none',
+		nextEffect: 'none',
+		margin: [ 10, 10, 0, 10 ],
+		padding: 0,
+		beforeShow: function() {
+            /* Right click disabled by default */
+            $.fancybox.wrap.bind("contextmenu", function() { return false; });
+        },
+		helpers: {
+			thumbs: { width: 50, height: 50 }
+		}
+	});
+});
