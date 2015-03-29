9caa3395ab9b27f2553ed0e74458466787963fc1
Mirko Pagliai <mirko.pagliai@gmail.com>
2015-03-29 22:16:59 +0200

    added methods

--- src/Utility/MePlugin.php
+++ src/Utility/MePlugin.php
@@ -23,6 +23,7 @@
 namespace MeTools\Utility;
 
 use Cake\Core\Plugin;
+use Cake\Filesystem\Folder;
 
 /**
  * An utility to handle plugins.
@@ -37,7 +38,7 @@ class MePlugin {
      * Alias for `getAll()` method
      * @see getAll()
      */
-    public function all() {
+    public static function all() {
         return call_user_func_array(array(get_class(), 'getAll'), func_get_args());
     }
 	
@@ -68,11 +69,74 @@ class MePlugin {
 		}, self::all());
 	}
 	
+	/**
+	 * Gets the version number for a plugin.
+	 * @param string $plugin Plugin name
+	 * @return mixed Version number or FALSE
+	 * @uses path()
+	 */
+	public static function getVersion($plugin) {
+		$path = self::path($plugin);
+		
+		if(empty($path))
+			return FALSE;
+		
+		$folder = new Folder($path);
+		$files = $folder->find('version(\.txt)?');
+		
+		return empty($files[0]) ? FALSE : trim(file_get_contents($path.$files[0]));
+	}
+	
+	/**
+	 * Gets the version number for each plugin.
+	 * @param string|array $except Plugins to exclude
+	 * @return mixed array with the version number for each plugin
+	 * @uses all()
+	 * @uses version()
+	 */
+	public static function getVersions($except = NULL) {
+		//Gets plugins
+		$plugins = self::all();
+		
+		//Removes exceptions
+		if(is_string($except) || is_array($except)) {
+			$except = is_array($except) ? $except : array($except);
+			$plugins = array_diff($plugins, $except);
+		}
+		
+		if(empty($plugins))
+			return FALSE;
+		
+		$versions = array();
+		
+		//For each plugin, sets the name and the version number
+		foreach($plugins as $plugin)
+			$versions[] = array('name' => $plugin, 'version' => self::version($plugin));
+		
+		return $versions;
+	}
+	
     /**
      * Alias for `getPath()` method
      * @see getPath()
      */
-    public function path() {
+    public static function path() {
         return call_user_func_array(array(get_class(), 'getPath'), func_get_args());
+    }
+	
+    /**
+     * Alias for `getVersion()` method
+     * @see getVersion()
+     */
+    public static function version() {
+        return call_user_func_array(array(get_class(), 'getVersion'), func_get_args());
+    }
+	
+    /**
+     * Alias for `getVersions()` method
+     * @see getVersions()
+     */
+    public static function versions() {
+        return call_user_func_array(array(get_class(), 'getVersions'), func_get_args());
     }
 }
