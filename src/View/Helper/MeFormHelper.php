7097c4d0c617bb28cec16b725d0028b74dd71592
Mirko Pagliai <mirko.pagliai@gmail.com>
2015-03-30 20:21:47 +0200

    added methods

--- src/View/Helper/MeFormHelper.php
+++ src/View/Helper/MeFormHelper.php
@@ -44,6 +44,77 @@ class MeFormHelper extends FormHelper {
 	public $helpers = ['Html' => ['className' => 'MeTools.MeHtml'], 'Url'];
 	
 	/**
+	 * Construct the widgets and binds the default context providers.
+	 * 
+	 * This method only ewrites the default configuration (`$_defaultConfig`).
+	 * @param Cake\View\View $view The View this helper is being attached to
+	 * @param array $config Configuration settings for the helper
+	 * @see http://api.cakephp.org/3.0/class-Cake.View.Helper.FormHelper.html#$_defaultConfig
+	 * @uses FormHelper::__construct()
+	 * @uses _defaultConfig
+	 */
+	public function __construct(View $view, $config = []) {
+		//Rewrites the default configuration
+		$this->_defaultConfig['templates'] = array_merge($this->_defaultConfig['templates'], [
+			'file' => '<input type="file" class="form-control" name="{{name}}"{{attrs}}>',
+			'input' => '<input type="{{type}}" class="form-control" name="{{name}}"{{attrs}}>',
+			'inputContainer' => '<div class="input form-group {{type}}{{required}}">{{content}}</div>',
+			'inputContainerError' => '<div class="input form-group {{type}}{{required}} has-error">{{content}}{{error}}</div>',
+			'select' => '<select class="form-control" name="{{name}}"{{attrs}}>{{content}}</select>',
+			'selectMultiple' => '<select class="form-control" name="{{name}}[]" multiple="multiple"{{attrs}}>{{content}}</select>',
+			'radio' => '<input class="form-control" type="radio" name="{{name}}" value="{{value}}"{{attrs}}>',
+			'textarea' => '<textarea class="form-control" name="{{name}}"{{attrs}}>{{value}}</textarea>',
+			'submitContainer' => '<div class="submit form-group">{{content}}</div>'
+		]);
+		
+        parent::__construct($view, $config);
+    }
+	
+	/**
+     * Creates a button.
+     * 
+     * This method creates a button. To create a POST button, you should use the `postButton()` method.
+     * Instead, to create a link with the appearance of a button, you should use the `button()` method provided by `MeHtmlHelper`.
+     * @param string $title The button label or an image
+     * @param array $options HTML attributes and options
+     * @return string Html code
+     * @see postButton(), MeHtmlHelper::button()
+	 * @uses MeHtmlHelper::_addButtonClass()
+	 * @uses MeHtmlHelper::_addDefault()
+	 * @uses MeHtmlHelper::_addIcon()
+	 */
+	public function button($title, array $options = []) {
+		$options = $this->Html->_addButtonClass($options);
+		$options = $this->Html->_addDefault('type', 'button', $options);
+		
+		$title = $this->Html->_addIcon($title, $options);
+        unset($options['icon']);
+		
+		return parent::button($title, $options);
+	}
+	
+	/**
+	 * Generates an input element complete with label and wrapper div.
+     * @param string $fieldName Field name, should be "Modelname.fieldname"
+     * @param array $options HTML attributes and options
+	 * @return string Html code
+	 * @uses MeHtmlHelper::_addDefault()
+	 */
+    public function input($fieldName, array $options = []) {
+		//If the field name contains the word "password", then the field type is "password"
+		if(preg_match('/password/', $fieldName))
+			$options = $this->Html->_addDefault('type', 'password', $options);
+		
+		//$type = self::_inputType($fieldName, $options);
+		
+		//Changes the "autocomplete" value from "FALSE" to "off"
+		if(isset($options['autocomplete']) && !$options['autocomplete'])
+			$options['autocomplete'] = 'off';
+		
+        return parent::input($fieldName, $options);
+	}
+	
+	/**
 	 * Creates a `<legend>` tag.
      * @param string $text Legend text
      * @param array $options HTML attributes and options
@@ -100,5 +171,18 @@ class MeFormHelper extends FormHelper {
 		$options = $this->Html->_addDefault('escapeTitle', FALSE, $options);
 
         return parent::postLink($title, $url, $options);
+	}
+	
+	/**
+     * Creates a submit button.
+     * @param string $caption The label appearing on the submit button or an image
+     * @param array $options HTML attributes and options
+     * @return string Html code
+	 * @uses MeHtmlHelper::_addButtonClass()
+	 */
+	public function submit($caption = null, array $options = []) {
+		$options = $this->Html->_addButtonClass($options, 'success');
+		
+		return parent::submit($caption, $options);
 	}
 }
