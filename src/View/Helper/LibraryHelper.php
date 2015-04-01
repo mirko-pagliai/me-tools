4e510aa72eaf3bb64a89a3e92dab0b6f566c1b82
Mirko Pagliai <mirko.pagliai@gmail.com>
2015-04-01 11:45:40 +0200

    added (empty) helpers

---
+++ src/View/Helper/LibraryHelper.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This file is part of MeTools.
+ *
+ * MeTools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * MeTools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with MeTools.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author		Mirko Pagliai <mirko.pagliai@gmail.com>
+ * @copyright	Copyright (c) 2015, Mirko Pagliai for Nova Atlantis Ltd
+ * @license		http://www.gnu.org/licenses/agpl.txt AGPL License
+ * @link		http://git.novatlantis.it Nova Atlantis Ltd
+ * @see			http://getbootstrap.com/components/#dropdowns Bootstrap documentation
+ */
+namespace MeTools\View\Helper;
+
+use Cake\View\Helper;
+use Cake\View\View;
+
+/**
+ * Library helper
+ */
+class LibraryHelper extends Helper {
+}
