d812d71e3bb32e04ff1046dc9522a66483906b04
Mirko Pagliai <mirko.pagliai@gmail.com>
2015-04-02 15:36:00 +0200

    added "am()" e "ac()" functions

---
+++ config/bootstrap.php
@@ -0,0 +1,26 @@
+<?php
+if(!function_exists('am')) {
+	/**
+	* Merge a group of arrays.
+	* Accepts variable arguments. Each argument will be converted into an array and then merged.
+	* @return array All array parameters merged into one
+	*/
+	function am() {
+		foreach(func_get_args() as $arg)
+			$array = array_merge(empty($array) ? [] : $array, is_array($arg) ? $arg : [$arg]);
+		
+		return $array;
+	}
+}
+
+if(!function_exists('ac')) {
+	/**
+	 * Cleans an array.
+	 * Removes empty values (`array_filter()`) and duplicates (`ærray_unique()`)
+	 * @param array $array Array
+	 * @return array Array
+	 */
+	function ac($array) {
+		return is_array($array) ? array_unique(array_filter($array)) : $array;
+	}
+}
