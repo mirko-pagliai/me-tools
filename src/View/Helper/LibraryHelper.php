e7447987ea8e870f99988e78362184fd7a8102a8
Mirko Pagliai <mirko.pagliai@gmail.com>
2015-05-06 12:26:48 +0200

    fixed bug for analitycs method

--- src/View/Helper/LibraryHelper.php
+++ src/View/Helper/LibraryHelper.php
@@ -111,8 +111,12 @@ class LibraryHelper extends Helper {
 	 * Create a script block for Google Analytics
 	 * @param string $id Analytics ID
 	 * @uses MeTools\View\Helper\MeHtmlHelper::scriptBlock()
+	 * @return mixed Html code or NULL if is localhost
 	 */
 	public function analytics($id) {
+		if(is_localhost())
+			return NULL;
+		
 		return $this->Html->scriptBlock(sprintf('!function(e,a,t,n,c,o,s){e.GoogleAnalyticsObject=c,e[c]=e[c]||function(){(e[c].q=e[c].q||[]).push(arguments)},e[c].l=1*new Date,o=a.createElement(t),s=a.getElementsByTagName(t)[0],o.async=1,o.src=n,s.parentNode.insertBefore(o,s)}(window,document,"script","//www.google-analytics.com/analytics.js","ga"),ga("create","%s","auto"),ga("send","pageview");', $id), ['block' => 'script_bottom']);
 	}
 	
