833c42a9ce55518a8a8fa8c04aeb64b84dc8441c
Mirko Pagliai <mirko.pagliai@gmail.com>
2016-01-03 12:29:29 +0100

    fixed

--- src/Utility/System.php
+++ src/Utility/System.php
@@ -66,24 +66,16 @@ class System {
 	 * It searchs into `ROOT` and all loaded plugins.
 	 * @uses MeTools\Core\Plugin::path()
 	 * @return array Changelog files
-	 * @uses Cake\I18n\I18n::locale()
 	 * @uses MeTools\Core\Plugin::path()
 	 */
 	public static function changelogs() {
-		$files = af(array_map(function($path) {
-			//Gets the current locale
-			$locale = substr(\Cake\I18n\I18n::locale(), 0, 2);
-
-			if(!empty($locale) && is_readable($file = sprintf($path.'CHANGELOG_%s.md', $locale)))
-				return str_replace(ROOT.DS, NULL, $file);
-			elseif(is_readable($file = $path.'CHANGELOG.md'))
-				return str_replace(ROOT.DS, NULL, $file);
-			else
-				return FALSE;
-		}, am([ROOT.DS], Plugin::path())));
+		$files = [];
 		
-		//Re-indexes, starting to 1, and returns
-		return array_combine(range(1, count($files)), array_values($files));
+		foreach(am([ROOT.DS], Plugin::path()) as $path)
+			foreach((new Folder($path))->find('CHANGELOG(\..+)?') as $file)
+				$files[] = str_replace(ROOT.DS, NULL, $path.$file);
+		
+		return $files;
 	}
 	
     /**
