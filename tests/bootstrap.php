ce7630a4f1d4d349158bedebf330810cd40943c8
Mirko Pagliai <mirko.pagliai@gmail.com>
2016-08-25 20:16:50 +0200

    added config for tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,12 +1,57 @@
 <?php
 /**
- * Test runner bootstrap.
+ * This file is part of MeTools.
  *
- * Add additional configuration/setup your application needs when running
- * unit tests in this file.
+ * MeTools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * MeTools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with MeTools.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author      Mirko Pagliai <mirko.pagliai@gmail.com>
+ * @copyright   Copyright (c) 2016, Mirko Pagliai for Nova Atlantis Ltd
+ * @license     http://www.gnu.org/licenses/agpl.txt AGPL License
+ * @link        http://git.novatlantis.it Nova Atlantis Ltd
  */
+use Cake\Core\Configure;
+use Cake\Datasource\ConnectionManager;
+
 require dirname(__DIR__) . '/vendor/autoload.php';
+
+// Path constants to a few helpful things.
+define('ROOT', dirname(__DIR__) . DS);
+define('CAKE_CORE_INCLUDE_PATH', ROOT . 'vendor' . DS . 'cakephp' . DS . 'cakephp');
+define('CORE_PATH', ROOT . 'vendor' . DS . 'cakephp' . DS . 'cakephp' . DS);
+define('CAKE', CORE_PATH . 'src' . DS);
+define('TESTS', ROOT . 'tests');
+define('APP', ROOT . 'tests' . DS . 'test_app' . DS);
+define('APP_DIR', 'test_app');
+define('WEBROOT_DIR', 'webroot');
+define('WWW_ROOT', APP . 'webroot' . DS);
+define('TMP', sys_get_temp_dir() . DS);
+define('CONFIG', APP . 'config' . DS);
+define('CACHE', TMP);
+define('LOGS', TMP);
 
 require dirname(__DIR__) . '/config/bootstrap.php';
 
-$_SERVER['PHP_SELF'] = '/';
+Configure::write('debug', true);
+
+// Ensure default test connection is defined
+if (!getenv('db_dsn')) {
+    putenv('db_dsn=sqlite://127.0.0.1/' . TMP . 'debug_kit_test.sqlite');
+}
+$config = [
+    'url' => getenv('db_dsn'),
+    'timezone' => 'UTC',
+];
+
+// Use the test connection for 'debug_kit' as well.
+ConnectionManager::config('test', $config);
