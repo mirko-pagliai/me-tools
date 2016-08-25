1631507f1c1d01e1c8414c5eb0343c0f5cb7fabf
Mirko Pagliai <mirko.pagliai@gmail.com>
2016-08-25 21:13:07 +0200

    fixed bug

--- config/global_functions.php
+++ config/global_functions.php
@@ -249,7 +249,7 @@ if (!function_exists('optionDefaults')) {
      * <code>
      * $options = optionDefaults([
      *  'class' => 'this-is-my-class',
-     *  'data-balue => 'example-value',
+     *  'data-value => 'example-value',
      * ], $options);
      * </code>
      *
@@ -261,7 +261,12 @@ if (!function_exists('optionDefaults')) {
      */
     function optionDefaults($values, $options)
     {
-        if (func_num_args() === 3) {
+        ///Backward compatibility with three arguments
+        if (func_num_args() === 3 &&
+            is_string(func_get_arg(0)) &&
+            is_string(func_get_arg(1)) &&
+            is_array(func_get_arg(2))
+        ) {
             $values = [func_get_arg(0) => func_get_arg(1)];
             $options = func_get_arg(2);
         }
