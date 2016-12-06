126c9f2df5fa33c66f7fb1d1ef7c140b9d7b13b1
Mirko Pagliai <mirko.pagliai@gmail.com>
2016-12-06 10:59:34 +0100

    added tests

---
+++ tests/TestCase/Shell/InstallShellTest.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * This file is part of MeTools.
+ *
+ * MeTools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * MeTools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with MeTools.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author      Mirko Pagliai <mirko.pagliai@gmail.com>
+ * @copyright   Copyright (c) 2016, Mirko Pagliai for Nova Atlantis Ltd
+ * @license     http://www.gnu.org/licenses/agpl.txt AGPL License
+ * @link        http://git.novatlantis.it Nova Atlantis Ltd
+ */
+namespace MeTools\Test\TestCase\Shell;
+
+use Cake\TestSuite\TestCase;
+use MeTools\Shell\InstallShell;
+
+/**
+ * InstallShellTest class
+ */
+class InstallShellTest extends TestCase
+{
+    /**
+     * @var \Cake\Shell\InstallShell
+     */
+    protected $InstallShell;
+
+    /**
+     * @var \Cake\Console\ConsoleIo
+     */
+    protected $io;
+
+    /**
+     * Setup the test case, backup the static object values so they can be
+     * restored. Specifically backs up the contents of Configure and paths in
+     *  App if they have not already been backed up
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->io = $this->getMockBuilder('Cake\Console\ConsoleIo')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->InstallShell = new InstallShell($this->io);
+    }
+
+    /**
+     * Teardown any static object changes and restore them
+     * @return void
+     */
+    public function tearDown()
+    {
+        parent::tearDown();
+
+        unset($this->InstallShell, $this->io);
+    }
+
+    /**
+     * Tests for `createRobots()` method
+     * @test
+     */
+    public function testCreateRobots()
+    {
+        $this->InstallShell->createRobots();
+
+        $this->assertFileExists(WWW_ROOT . 'robots.txt');
+        $this->assertEquals(
+            file_get_contents(WWW_ROOT . 'robots.txt'),
+            'User-agent: *' . PHP_EOL . 'Disallow: /admin/' . PHP_EOL .
+            'Disallow: /ckeditor/' . PHP_EOL . 'Disallow: /css/' . PHP_EOL .
+            'Disallow: /js/' . PHP_EOL . 'Disallow: /vendor/'
+        );
+
+        unlink(WWW_ROOT . 'robots.txt');
+    }
+
+    /**
+     * Test for `getOptionParser()` method
+     * @test
+     */
+    public function testGetOptionParser()
+    {
+        $parser = $this->InstallShell->getOptionParser();
+
+        $this->assertEquals('Cake\Console\ConsoleOptionParser', get_class($parser));
+        $this->assertEquals([
+            'all',
+            'copyConfig',
+            'copyFonts',
+            'createDirectories',
+            'createRobots',
+            'createVendorsLinks',
+            'fixComposerJson',
+            'installPackages',
+            'setPermissions',
+        ], array_keys($parser->subcommands()));
+        $this->assertEquals('Executes some tasks to make the system ready to work', $parser->description());
+        $this->assertEquals(['force', 'help', 'quiet', 'verbose'], array_keys($parser->options()));
+    }
+}
