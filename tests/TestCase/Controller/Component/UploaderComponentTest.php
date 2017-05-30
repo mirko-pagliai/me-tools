14d106f06a33c2bb71d6b378e84e369f53f16752
mirko-pagliai <mirko.pagliai@gmail.com>
2017-05-30 15:01:19 +0200

    fixed test

--- tests/TestCase/Controller/Component/UploaderComponentTest.php
+++ tests/TestCase/Controller/Component/UploaderComponentTest.php
@@ -168,11 +168,11 @@ class UploaderComponentTest extends TestCase
         ], array_keys((array)$this->Uploader->file));
 
         $this->Uploader->set(array_merge($file, ['error' => UPLOAD_ERR_INI_SIZE]));
-        $this->assertNotEmpty($this->Uploader->file);
+        $this->assertInstanceOf('stdClass', $this->Uploader->file);
         $this->assertNotEmpty($this->Uploader->error());
 
         $this->Uploader->set(array_merge($file, ['error' => 'noExistingErrorCode']));
-        $this->assertNotEmpty($this->Uploader->file);
+        $this->assertInstanceOf('stdClass', $this->Uploader->file);
         $this->assertEquals('Unknown upload error', $this->Uploader->error());
     }
 
