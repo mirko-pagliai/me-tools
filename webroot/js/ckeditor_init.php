6eb401ca9498b46d96234e44fe4e4ab702d8a571
mirko-pagliai <mirko.pagliai@gmail.com>
2017-09-25 12:34:48 +0200

    fixed align classes for images on ckeditor

--- webroot/js/ckeditor_init.php
+++ webroot/js/ckeditor_init.php
@@ -17,7 +17,7 @@ $(function () {
             disableNativeSpellChecker: false,
             fontSize_sizes: '10/10px;11/11px;12/12px;14/14px;16/16px;18/18px;20/20px;22/22px;24/24px;26/26px;28/28px;30/30px;',
             height: 375,
-            image2_alignClasses: [ 'pull-left', 'text-center', 'pull-right' ],
+            image2_alignClasses: [ 'float-left', 'text-center', 'float-right' ],
             insertpre_class: false,
             removeButtons: 'Font',
             removeDialogTabs: false,
