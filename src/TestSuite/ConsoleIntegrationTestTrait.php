19ea1ba60dfa07418519f098ac414bb6d2cd055d
mirko-pagliai <mirko.pagliai@gmail.com>
2018-12-10 17:25:37 +0100

    fixed

--- src/TestSuite/ConsoleIntegrationTestTrait.php
+++ src/TestSuite/ConsoleIntegrationTestTrait.php
@@ -9,7 +9,7 @@
  * @copyright   Copyright (c) Mirko Pagliai
  * @link        https://github.com/mirko-pagliai/me-tools
  * @license     https://opensource.org/licenses/mit-license.php MIT License
- * @since       2.18
+ * @since       2.18.0
  */
 namespace MeTools\TestSuite;
 
