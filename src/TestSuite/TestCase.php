bed0b6de2f704dfb37820557f65276dec665969a
mirko-pagliai <mirko.pagliai@gmail.com>
2019-08-05 17:06:05 +0200

    fixed bug for `TestCase::tearDown()` method

--- src/TestSuite/TestCase.php
+++ src/TestSuite/TestCase.php
@@ -19,6 +19,7 @@ use Exception;
 use MeTools\TestSuite\MockTrait;
 use Tools\ReflectionTrait;
 use Tools\TestSuite\TestTrait;
+use Symfony\Component\Filesystem\Exception\IOException;
 
 /**
  * TestCase class
@@ -48,13 +49,15 @@ abstract class TestCase extends CakeTestCase
     {
         parent::tearDown();
 
-        if (LOGS !== TMP) {
-            @unlink_recursive(LOGS, 'empty');
+        try {
+            if (LOGS !== TMP) {
+                unlink_recursive(LOGS, 'empty');
+            }
+            unlink_recursive(WWW_ROOT . 'vendor', 'empty');
+            unlink(WWW_ROOT . 'me_tools');
+            unlink(WWW_ROOT . 'robots.txt');
+        } catch (IOException $e) {
         }
-
-        @unlink_recursive(WWW_ROOT . 'vendor', 'empty');
-        @unlink(WWW_ROOT . 'me_tools');
-        @unlink(WWW_ROOT . 'robots.txt');
     }
 
     /**
