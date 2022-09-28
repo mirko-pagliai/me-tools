5584a1c4234754ca557f15689f40008b8c865090
mirko-pagliai <mirko.pagliai@gmail.com>
2022-09-28 19:53:02 +0200

    come back

--- tests/TestCase/View/Helper/HtmlHelperTest.php
+++ tests/TestCase/View/Helper/HtmlHelperTest.php
@@ -30,6 +30,7 @@ class HtmlHelperTest extends HelperTestCase
      * Tests for `__call()` method
      * @deprecated
      * @test
+     * @noinspection PhpUndefinedMethodInspection
      */
     public function testCall(): void
     {
@@ -53,7 +54,6 @@ class HtmlHelperTest extends HelperTestCase
         //With a no existing method
         $this->expectException(ErrorException::class);
         $this->expectExceptionMessage('Method `' . HtmlHelper::class . '::noExistingMethod()` does not exist');
-        /** @noinspection PhpUndefinedMethodInspection */
         $this->Helper->noExistingMethod(null, null, null);
     }
 
