b1c0e5154c611cf90f7f475a823f192ab6881203
mirko-pagliai <mirko.pagliai@gmail.com>
2022-10-05 19:16:25 +0200

    fixed

--- tests/TestCase/Command/Install/CreatePluginsLinksCommandTest.php
+++ tests/TestCase/Command/Install/CreatePluginsLinksCommandTest.php
@@ -32,10 +32,7 @@ class CreatePluginsLinksCommandTest extends TestCase
      */
     public function testExecute(): void
     {
-        //@todo it can be simplified
-        $clear = fn() => array_map(function (string $path): bool {
-            return file_exists($path) && Filesystem::instance()->rmdirRecursive($path);
-        }, [WWW_ROOT . 'me_tools', WWW_ROOT . 'test_plugin']);
+        $clear = fn() => array_map(fn(string $path): bool => file_exists($path) && Filesystem::instance()->rmdirRecursive($path), [WWW_ROOT . 'me_tools', WWW_ROOT . 'test_plugin']);
 
         $this->loadPlugins(['TestPlugin' => []]);
 
