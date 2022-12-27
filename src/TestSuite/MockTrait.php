8fb5a25d6be6d9018759b667dd8fbb072cee5ec8
mirko-pagliai <mirko.pagliai@gmail.com>
2022-12-27 16:18:53 +0100

    fixed

--- src/TestSuite/MockTrait.php
+++ src/TestSuite/MockTrait.php
@@ -43,15 +43,12 @@ trait MockTrait
     }
 
     /**
-     * Gets the alias name for which a test is being performed, starting from a
-     *  class or a `TestCase` class.
+     * Gets the alias name for which a test is being performed, starting from a class or a `TestCase` class.
      *
-     * Example: class `MyPlugin\Test\TestCase\Controller\PagesControllerTest`
-     *  will return the string `Pages`.
-     * @param class-string|object $class Class name or object
+     * Example: class `MyPlugin\Test\TestCase\Controller\PagesControllerTest`  will return `Pages`.
+     * @param class-string<\PHPUnit\Framework\TestCase>|\PHPUnit\Framework\TestCase $class Class name or object
      * @return string
-     * @throws \PHPUnit\Framework\AssertionFailedError|\ReflectionException If the
-     *  class does not exist or if it is impossible to get its alias
+     * @throws \PHPUnit\Framework\AssertionFailedError|\ReflectionException
      * @since 2.19.9
      */
     protected function getAlias($class): string
@@ -115,14 +112,12 @@ trait MockTrait
     }
 
     /**
-     * Gets the class name for which a test is being performed, starting from a
-     *  `TestCase` class.
+     * Gets the class name for which a test is being performed, starting from a `TestCase` class.
      *
-     * Example: class `MyPlugin\Test\TestCase\Controller\PagesControllerTest`
-     *  will return the string `MyPlugin\Controller\PagesController`.
+     * Example: class `MyPlugin\Test\TestCase\Controller\PagesControllerTest` will return `MyPlugin\Controller\PagesController`.
      * @param \PHPUnit\Framework\TestCase $className A `TestCase` class
-     * @return class-string<\PHPUnit\Framework\TestCase>|string The class name for
-     *  which a test is being performed. Empty string on failure
+     * @return class-string<\PHPUnit\Framework\TestCase>|string The class name for which a test is being performed or
+     *  empty string on failure
      * @since 2.18.0
      */
     protected function getOriginClassName(TestCase $className): string
@@ -133,15 +128,13 @@ trait MockTrait
     }
 
     /**
-     * Gets the class name for which a test is being performed, starting from a
-     *  `TestCase` class.
+     * Gets the class name for which a test is being performed, starting from a `TestCase` class.
      *
      * It fails if the class cannot be determined or does not exist.
      * @param \PHPUnit\Framework\TestCase $className A `TestCase` class
      * @return class-string The class name for which a test is being performed
      * @since 2.19.2
-     * @throw \PHPUnit\Framework\AssertionFailedError If the class does not
-     *  exist or if it is impossible to get its origin class name
+     * @throw \PHPUnit\Framework\AssertionFailedError
      */
     protected function getOriginClassNameOrFail(TestCase $className): string
     {
@@ -163,11 +156,9 @@ trait MockTrait
     }
 
     /**
-     * Gets the classname for which a test is being performed, starting from a
-     *  `TestCase` class.
+     * Gets the classname for which a test is being performed, starting from a `TestCase` class.
      *
-     * Example: class `MyPlugin\MySubNamespace\Test\TestCase\MyExampleTest`
-     *  will return the string `MyPlugin/MySubNamespace`.
+     * Example: class `MyPlugin\MySubNamespace\Test\TestCase\MyExampleTest` will return `MyPlugin/MySubNamespace`.
      * @param \PHPUnit\Framework\TestCase $testClass A `TestCase` class
      * @return string
      * @since 2.19.9
