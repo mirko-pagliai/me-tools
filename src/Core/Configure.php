0ad3411be0753a5095e8f72a91592a656542f0f3
mirko-pagliai <mirko.pagliai@gmail.com>
2023-04-27 15:25:01 +0200

    added `Configure` class, with `readFromPlugins()` metho

---
+++ src/Core/Configure.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of me-tools.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright   Copyright (c) Mirko Pagliai
+ * @link        https://github.com/mirko-pagliai/me-tools
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ * @since       2.23.3
+ */
+
+namespace MeTools\Core;
+
+use Cake\Core\Configure as BaseConfigure;
+
+/**
+ * Configuration class. Used for managing runtime configuration information
+ */
+class Configure extends BaseConfigure
+{
+    /**
+     * Used to read information stored in Configure.
+     *
+     * This method looks for the same configuration key in all loaded plugins.
+     *
+     * For example, if the plugins are `PluginOne` and `PluginTwo` and `$var` is `myValue`, it will return the values
+     *  of `PluginOne.myValue` and `PluginTwo.myValue`, if they exist.
+     * @param string $var Variable name
+     * @return array<string, mixed>
+     */
+    public static function readFromPlugins(string $var): array
+    {
+        $plugins = array_filter(Plugin::loaded(), fn(string $plugin): bool => Configure::check($plugin . '.' . $var));
+
+        return array_combine($plugins, array_map(fn(string $plugin) => Configure::read($plugin . '.' . $var), $plugins));
+    }
+}
