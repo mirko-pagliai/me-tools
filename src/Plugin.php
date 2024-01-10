5aaff524c003f3e1c26fd5f2e02c1e37c0b2acea
mirko-pagliai <mirko.pagliai@gmail.com>
2024-01-10 19:24:15 +0100

    updated

--- src/Plugin.php
+++ src/Plugin.php
@@ -18,11 +18,6 @@ namespace MeTools;
 use Cake\Console\CommandCollection;
 use Cake\Core\BasePlugin;
 use Cake\Core\PluginApplicationInterface;
-use MeTools\Command\Install\CreateDirectoriesCommand;
-use MeTools\Command\Install\CreateRobotsCommand;
-use MeTools\Command\Install\CreateVendorsLinksCommand;
-use MeTools\Command\Install\RunAllCommand;
-use MeTools\Command\Install\SetPermissionsCommand;
 
 /**
  * Plugin class
@@ -41,19 +36,5 @@ class Plugin extends BasePlugin
         parent::bootstrap($app);
 
         $app->addOptionalPlugin('Assets');
-    }
-
-    /**
-     * Add console commands for the plugin
-     * @param \Cake\Console\CommandCollection $commands The command collection to update
-     * @return \Cake\Console\CommandCollection
-     */
-    public function console(CommandCollection $commands): CommandCollection
-    {
-        return $commands->add('me_tools.create_directories', CreateDirectoriesCommand::class)
-            ->add('me_tools.create_robots', CreateRobotsCommand::class)
-            ->add('me_tools.create_vendors_links', CreateVendorsLinksCommand::class)
-            ->add('me_tools.install', RunAllCommand::class)
-            ->add('me_tools.set_permissions', SetPermissionsCommand::class);
     }
 }
